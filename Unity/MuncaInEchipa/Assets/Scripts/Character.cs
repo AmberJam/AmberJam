d27543504168fe2ce9e02bf172ead6434175704d
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2015-12-14 21:36:34 +0200

    Added Need and character scripts

---
+++ Unity/MuncaInEchipa/Assets/Scripts/Character.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System;
+
+public enum HappinessLevel { Mort, Suparat, Meh, Woohoo};
+
+public class Character : MonoBehaviour {
+
+    public List<Need> allNeeds;
+    private string charName;
+    private List<Text> displayNeeds;
+    public HappinessLevel Happiness;
+
+    public int depletedUpdateRate = 10;
+    public int needsUpdateRate = 1;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateAllNeeds(Time.deltaTime);
+	}
+
+    void SatisfyNeed(NeedType type)
+    {
+        foreach(Need need in allNeeds)
+        {
+            if(need.type == type)
+            {
+                need.SatisfyNeed();
+
+                affectNeedsBySatisfying(need.affectedTypes);
+            }
+        }
+    }
+
+    void affectNeedsBySatisfying(List<AffectedNeed> affectedNeeds)
+    {
+        foreach(AffectedNeed affNeed in affectedNeeds)
+        {
+            foreach(Need need in allNeeds)
+            {
+                if(need.type == affNeed.type)
+                {
+                    need.decreaseCooldown(affNeed.affectedAmount);
+                }
+            }
+        }
+    }
+
+    //vede daca pierzi happiness
+    void UpdateAllNeeds(float dt)
+    {
+        foreach (Need need in allNeeds)
+        {
+            if(need.MyUpdate(dt) >= depletedUpdateRate)
+            {
+                need.resetLastTimeDepleted();
+                if(Happiness > HappinessLevel.Mort)
+                    Happiness--;
+            }
+        }
+    }
+}
---
+++ Unity/MuncaInEchipa/Assets/Scripts/Need.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public enum NeedType { NoType};
+
+[Serializable]
+public struct AffectedNeed
+{
+    public NeedType type;
+    public int affectedAmount; //cu cat e afectat need-ul
+};
+
+public class Need : MonoBehaviour {
+    
+
+
+    public float defaultCooldown;
+    private float currentCooldown;
+    public int ratioPerSec = 1;
+    public NeedType type = NeedType.NoType;
+    public List<AffectedNeed> affectedTypes;
+    public DateTime lastTimeDepleted;
+
+    [NonSerialized]
+    public bool wasDepleted = false;
+
+    // Use this for initialization
+    void Start () {
+        lastTimeDepleted = DateTime.Now;
+        currentCooldown = defaultCooldown;
+        affectedTypes = new List<AffectedNeed>();
+        wasDepleted = false;
+	}
+
+    public void decreaseCooldown(float amount)
+    {
+        currentCooldown -= amount;
+    }
+	
+	// Update is called once per frame
+	public int MyUpdate (float dt) {
+
+        decreaseCooldown(dt * ratioPerSec);
+
+        if (currentCooldown <= 0f)
+        {
+            currentCooldown = 0f;
+            if(!wasDepleted)
+            {
+                wasDepleted = true;
+                lastTimeDepleted = DateTime.Now;    
+            }
+        }
+
+        if (wasDepleted)
+            return (DateTime.Now - lastTimeDepleted).Seconds;
+        return 0;
+	}
+
+
+    public void resetLastTimeDepleted()
+    {
+        lastTimeDepleted = DateTime.Now;
+    }
+
+    public void SatisfyNeed()
+    {
+        wasDepleted = false;
+        currentCooldown = defaultCooldown;
+    }
+}
