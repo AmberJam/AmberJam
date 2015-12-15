0f18fa1d98cd3ed9c5c4ba7ff68007f2ad20f361
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2015-12-15 05:12:58 +0200

    added time bar

---
+++ Unity/MuncaInEchipa/Assets/Scripts/TimeBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class TimeBar : MonoBehaviour {
+
+    public int numSeconds = 300;
+    private int currentProgress = 0;
+    private RawImage fullProgressBar;
+    private Vector2 initialSize;
+    private DateTime startTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = DateTime.Now;
+        fullProgressBar = GetComponent<RawImage>();
+        initialSize = fullProgressBar.rectTransform.sizeDelta;
+    }
+
+    void Update()
+    {
+
+        fullProgressBar.rectTransform.sizeDelta = Vector2.Lerp(new Vector2(0, initialSize.y), initialSize, (float)(DateTime.Now - startTime).TotalSeconds / numSeconds);
+        
+
+    }
+
+}
