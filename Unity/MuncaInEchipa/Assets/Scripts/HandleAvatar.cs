7848dd0c6a21b915cdcf0cab32f52ad68698d8c7
Daniel Alexandru Radu <danielalexandru.radu@gmail.com>
2015-12-15 09:10:13 +0200

    implemented new faces

---
+++ Unity/MuncaInEchipa/Assets/Scripts/HandleAvatar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HandleAvatar : MonoBehaviour {
+
+    public List<Texture> avatars;
+
+    Character associatedChar;
+    UnityEngine.UI.RawImage rawImg;
+
+    void Start()
+    {
+        rawImg = transform.FindChild("Fata").GetComponent<UnityEngine.UI.RawImage>();
+        associatedChar = GetComponent<Character>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (associatedChar.Happiness == HappinessLevel.Mort)
+            rawImg.texture = avatars[0];
+        else if (associatedChar.Happiness == HappinessLevel.Suparat)
+            rawImg.texture = avatars[1];
+        else if (associatedChar.Happiness == HappinessLevel.Meh)
+            rawImg.texture = avatars[2];
+        else if (associatedChar.Happiness == HappinessLevel.Woohoo)
+            rawImg.texture = avatars[3];
+	}
+}
